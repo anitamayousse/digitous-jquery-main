97a556446dc9718135713d18260f85b1ef53a811
Apple <anita.mayousse@gmail.com>
2022-01-18 17:23:37 +0100

    ex23

--- exercice32/js/main.js
+++ exercice32/js/main.js
@@ -1,5 +1,4 @@
-/* 1st method */
-/*$("button").click(function () {
+$("button").click(function () {
     $.ajax({
       url:"https://restcountries.com/v3.1/name/france",
       success: function (data) {
@@ -14,28 +13,4 @@
         })
       }
     });
-  }); */
-
-/*2nd method done by fetch*/
-  $("button").click(function() {
-    getCountries()
-    .then(res =>{
-      res.forEach(country => {
-        
-        $("#exercise").append(  
-        `<p> ${country.capital}</p>`,
-        `<p> ${country.name.common}</p>`
-        )
-         
-      });
-      
-    })
   });
-  
-  
-  async function getCountries(){
-    const response = await fetch(`https://restcountries.com/v3.1/name/france`);
-    const responseData = await response.json();
-  
-    return responseData;
-  }
